06a3329e6a75aea5cbf3bf1d474272e212abc705
sweerasingha <130553154+sweerasingha@users.noreply.github.com>
2023-09-22 11:02:37 +0000

    Changed the variable names. Comments added. Reduced 3 functions into one function. EG_2020_4267

--- travel_cost_calculator.py
+++ travel_cost_calculator.py
@@ -1,49 +1,59 @@
-from csv import *
+#EG_2020_4267
 
-a = {}
-b = {}
-c = {}
+import csv
 
-def lhr(file):  
-    with open(file) as h:
-        r = reader(h)
-        for row in r:
-            a[row[0]] = float(row[1])
+# Define dictionaries to store data
+hotel_rates = {}
+exchange_rates = {}
+flight_costs = {}
 
-def ler(file): 
-    with open(file) as e:
-        r = reader(e)
-        for row in r:
-            b[row[0].upper()] = float(row[1]) * 1 
-
-def lfr(file):
-    with open(file) as f:
-        r = reader(f)
-        for row in r:
-            c[row[0]] = float(row[1])
+# Load data from CSV files into dictionaries
+def load_data(file, data_dict):
+    with open(file, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        next(reader)  # Skip header row
+        for row in reader:
+            data_dict[row[0].upper()] = float(row[1])
 
 def main():
-    lhr('data/hotel_rates.csv')
-    ler('data/exchange_rates.csv')
-    lfr('data/flight_costs.csv')
+    # Load data from CSV files
+    load_data('data/hotel_rates.csv', hotel_rates)
+    load_data('data/exchange_rates.csv', exchange_rates)
+    load_data('data/flight_costs.csv', flight_costs)
 
-    d = input("Enter your destination: ").upper()
+    # Get destination input from user
+    destination = input("Enter your destination: ").upper()
 
-    f = c.get(d, 0.0)
-    h = a.get(d, 0.0)
+    # Calculate hotel and flight costs
+    hotel_cost = hotel_rates.get(destination, 0.0)
+    flight_cost = flight_costs.get(destination, 0.0)
 
-    days = int(input("Enter your stay duration in days: "))
-    h *= days
-    total = f + h
+    # Get stay duration from user
+    try:
+        days = int(input("Enter your stay duration in days: "))
+    except ValueError:
+        print("Invalid input for stay duration. Please enter a valid number.")
+        return
 
-    print(f"Flight cost: USD {f:.2f}")
-    print(f"Hotel cost for {days} days: USD {h:.2f}")
-    print(f"Total: USD {total:.2f}")
+    # Calculate total cost
+    total_cost = (hotel_cost * days) + flight_cost
 
-    currency = input(f"Select your currency for final price estimation ({', '.join(b.keys())}): ")
+    print(f"Flight cost: USD {flight_cost:.2f}")
+    print(f"Hotel cost for {days} days: USD {hotel_cost * days:.2f}")
+    print(f"Total: USD {total_cost:.2f}")
 
-    p = total * b[currency]
-    print(f"Total in {currency}: {p:.2f}")
+    # Get currency input from user
+    currency = input(f"Select your currency for final price estimation ({', '.join(exchange_rates.keys())}): ").upper()
+
+    # Check if selected currency is available
+    if currency not in exchange_rates:
+        print("Selected currency is not supported.")
+        return
+
+    # Convert total cost to selected currency
+    converted_cost = total_cost * exchange_rates[currency]
+
+    print(f"Total in {currency}: {converted_cost:.2f}")
 
 if __name__ == "__main__":
     main()
